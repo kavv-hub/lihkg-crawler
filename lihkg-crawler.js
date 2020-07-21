02bd198a7321141ce8103e37cccfe0a8abd16935
Kavin Wong <38276947+kavv-hub@users.noreply.github.com>
2020-07-21 21:55:58 +0800

    Revert "[Release] v1.0.1"

--- lihkg-crawler.js
+++ lihkg-crawler.js
@@ -1,17 +1,16 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const { crawlByThread } = require('./src/crawler');
+const { crawlByThread } = require('./lib/crawler');
 
 program.version("1.0.0").description("Lihkg Web Crawler")
 
 program.command("t <thread>")
     .alias("thread")
     .description("Crawl by thread")
-    .action(thread => {
-        crawlByThread(thread)
-            .then(content => console.log(JSON.stringify(content)))
-            .finally(() => process.exit(1))
+    .action(async thread => {
+        console.log(JSON.stringify(await crawlByThread(thread)))
+        process.exit(1)
     })
 
 program.parse(process.argv)
